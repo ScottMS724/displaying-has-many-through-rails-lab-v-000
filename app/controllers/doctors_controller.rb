c4347e1148521d7e2b83049453d7bd6f3d9e4963
Scott Silverman <scottms724@gmail.com>
2019-10-04 14:22:06 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -8,4 +8,34 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
   end
   
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.create(doctor_params)
+    redirect_to doctor_path(@doctor)
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    @doctor.update(doctor_params)
+    redirect_to doctor_path(@doctor)
+  end
+
+  def destroy
+    @doctor = Doctor.find(parmams[:id])
+    @doctor.destroy
+    redirect_to doctor_path
+  end
+
+  private
+  def doctor_params
+    params.require(:doctor).permit(:name, :department)
+  end
+  
 end
